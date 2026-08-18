56b8bdc97161f7427415ce164c6a9dc1fb9ccde1
Adarsh <adarshbaraily05@gmail.com>
2026-08-18 22:00:42 +0530

    Added basic for loop program

---
+++ forloop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+    int i;
+    for (i = 0; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+    char ch;
+    for (ch = 'A'; ch <='Z'; ch++) {
+        printf("%c\n", ch);
+    }
+    return 0;
+}
+
