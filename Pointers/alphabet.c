cef5f418fccae1065c9fb04d19b9e3d3cf88971a
Adarsh <adarshbaraily05@gmail.com>
2026-08-25 23:40:26 +0530

    Organzied all  Pointer programs into one folder

---
+++ Pointers/alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    char str[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char *ptr = str;
+
+    printf("The alphabets of english language are : %s", ptr);
+
+    return 0;
+}
